453554da0b0c0fa55416a8f8e470acfa981f027e
Tania Voronchuk <tanya.perfekt.pr@gmail.com>
2022-07-22 00:52:19 +0300

    homework #1 added

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # goit-nodejs-hw-01
+
+Весь сисок контактів
+
+node index.js --action list
+https://monosnap.com/file/6KYKmWkDflrAfUOYfO2XS0ggCrqrpA
+
+Контакт по id
+
+node index.js --action get --id 5
+https://monosnap.com/file/AHoyodW69sUrTucG5uo1b5ZpK9xn7v
+
+Додавання контакту
+
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+https://monosnap.com/file/9rgkryRryduhR0ApTvpMzVgTaPa3wi
+
+Видалення контакту
+
+node index.js --action remove --id=3
+https://monosnap.com/file/KkL0DrvOn7f3hIpvpKutefKuad7kIq
---
+++ index.js
@@ -0,0 +1,41 @@
+const contacts = require("./contacts.js");
+const { program } = require("commander");
+
+program
+  .option("-a, --action, <type>", "data operation")
+  .option("-i, --id, <type>", "contact id")
+  .option("-n, --name, <type>", "contact name")
+  .option("-e, --email, <type>", "contact email")
+  .option("-p, --phone, <type>", "contact phone")
+
+  program.parse(process.argv);
+  const options = program.opts();
+
+const invokeAction = async({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'list':
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+
+    case 'get':
+      const getContact = await contacts.getContactById(id);
+      console.log(getContact);
+      break;
+
+    case 'add':
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case 'remove':
+       const removeContact = await contacts.removeContact(id);
+       console.log(removeContact);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(options);
